52382d33e05fe5938617aa793101f19f92feb447
Shengda <dingshengda@gmail.com>
2020-08-29 23:26:42 -0700

    Add test coverage for fee receiver

--- contracts/mock/MockToken.sol
+++ contracts/mock/MockToken.sol
@@ -13,8 +13,4 @@ contract MockToken is ERC20 {
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
-
-    function burn(address account, uint256 amount) public {
-        _burn(account, amount);
-    }
 }
