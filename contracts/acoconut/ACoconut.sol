a746ca8cd17f203c998231197197ebd90a29f999
Shengda <dingshengda@gmail.com>
2020-09-16 17:16:35 -0700

    Complete ac and acBTC contracts

--- contracts/acoconut/ACoconut.sol
+++ contracts/acoconut/ACoconut.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.6.8;
 
-contract ACoconut {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev ACoconut token.
+ */
+contract ACoconut is ERC20 {
+    
+    address governance;
+    mapping(address => bool) minters;
+
+    constructor() public ERC20("ACoconut", "AC") {
+        governance = msg.sender;
+    }
+
+    function setMinter(address user, bool allowed) public {
+        require(msg.sender == governance, "ACoconut: not governance");
+        minters[user] = allowed;
+    }
+
+    function mint(address user, uint256 amount) public {
+        require(minters[msg.sender], "ACoconut: not minter");
+        _mint(user, amount);
+    }
+
+    function burn(address user, uint256 amount) public {
+        require(minters[msg.sender], "ACoconut: not minter");
+        _burn(user, amount);
+    }
+}
--- contracts/acoconut/ACoconutBTC.sol
+++ contracts/acoconut/ACoconutBTC.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.6.8;
 
-contract ACoconutBTC {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev ACoconut BTC token.
+ */
+contract ACoconutBTC is ERC20 {
+    
+    address governance;
+    mapping(address => bool) minters;
+
+    constructor() public ERC20("ACoconut BTC", "acBTC") {
+        governance = msg.sender;
+    }
+
+    function setMinter(address user, bool allowed) public {
+        require(msg.sender == governance, "ACoconutBTC: not governance");
+        minters[user] = allowed;
+    }
+
+    function mint(address user, uint256 amount) public {
+        require(minters[msg.sender], "ACoconutBTC: not minter");
+        _mint(user, amount);
+    }
+
+    function burn(address user, uint256 amount) public {
+        require(minters[msg.sender], "ACoconutBTC: not minter");
+        _burn(user, amount);
+    }
+}
