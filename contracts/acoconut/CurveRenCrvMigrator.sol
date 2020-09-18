20470cd5bdfca2730a1cb307f3e8c17a03cbda4a
Shengda <dingshengda@gmail.com>
2020-09-18 02:26:49 -0700

    Complete ren crv migrator

--- contracts/acoconut/CurveRenCrvMigrator.sol
+++ contracts/acoconut/CurveRenCrvMigrator.sol
@@ -1,15 +1,45 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.6.8;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "../libraries/curve/ICurveFi.sol";
 import "./IMigrator.sol";
+import "./ACoconutExchange.sol";
 
 contract CurveRenCrvMigrator is IMigrator {
+    using SafeERC20 for IERC20;
 
     address public constant override want = address(0x49849C98ae39Fff122806C06791Fa73784FB3675); // renCrv token
     address public constant override get = address(0);  // To be added after ACoconutBTC is deployed
+    address public constant wbtc = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);    // WBTC token
+    address public constant renbtc = address(0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D);  // renBTC token
+    address public constant exchange = address(0); // To be added after ACoconutExchange is deployed
+    address public constant vault = address(0);    // To be added after ACoconutVault is deployed
+    address public constant curve = address(0x93054188d876f558f4a66B2EF1d97d16eDf0895B); // REN swap
 
     /**
      * @dev Performs the token migration.
      */
-    function migrate() public override {}
+    function migrate() public override {
+        require(msg.sender == vault, "not vault");
+        uint256 _want = IERC20(want).balanceOf(address(this));
+        if (_want > 0) {
+            ICurveFi(curve).remove_liquidity(_want, [uint256(0), 0]);
+        }
+        uint256 _wbtc = IERC20(wbtc).balanceOf(address(this));
+        uint256 _renbtc = IERC20(renbtc).balanceOf(address(this));
+        if (_wbtc > 0 || _renbtc > 0) {
+            // WBTC is the first token and renBTC is the second!
+            uint256[] memory amounts = new uint256[](2);
+            amounts[0] = _wbtc;
+            amounts[1] = _renbtc;
+            ACoconutExchange(exchange).mint(amounts, 0);
+        }
+        uint256 _get = IERC20(get).balanceOf(address(this));
+        if (_get > 0) {
+            IERC20(get).safeTransfer(vault, _get);
+        }
+    }
 }
