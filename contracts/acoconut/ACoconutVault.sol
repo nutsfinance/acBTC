6821599f81cf7f5d48b7b5f5c0b758f031cb964e
Shengda <dingshengda@gmail.com>
2020-09-19 00:51:56 -0700

    Updates ACoconut Vault

--- contracts/acoconut/ACoconutVault.sol
+++ contracts/acoconut/ACoconutVault.sol
@@ -11,53 +11,54 @@ import "../libraries/vaults/IStrategy.sol";
 import "../libraries/vaults/RewardedVault.sol";
 import "./IMigrator.sol";
 
+/**
+ * @notice A rewarded vault that could perform token migration.
+ */
 contract ACoconutVault is RewardedVault {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
-    // When the vault is ended. Migration can be done only after the vault is ended.
-    // After the vault is ended, no new deposit is allowed but user can withdraw.
-    uint256 vaultEndTimestamp;
-    // Whether migration is completed.
+    // When the vault can be migrated.
+    uint256 migrationDue;
+    // Whether the vault is migrated.
     bool migrated;
+    // The contract that performs the migration.
     address migrator;
 
-    constructor(uint256 _vaultDuration, address _rewardToken, address _vaultToken) public RewardedVault(_rewardToken, _vaultToken) {
-        require(_vaultDuration > 0, "ACoconutVault: Duration not set");
-        vaultEndTimestamp = block.timestamp + _vaultDuration;
+    constructor(uint256 _migrationDue, address _rewardToken, address _vaultToken) public RewardedVault(_rewardToken, _vaultToken) {
+        migrationDue = _migrationDue;
     }
 
     /**
      * @dev Updates the migrator address.
      */
     function setMigrator(address _migrator) public {
-        require(msg.sender == governance, "ACoconutVault: not governance");
-        require(_migrator != address(0x0), "ACoconutVault: migrator not set");
-        require(!migrated, "ACoconutVault: migrated");
-        require(IMigrator(_migrator).want() == address(token), "ACoconutVault: different token");
+        require(msg.sender == governance, "not governance");
+        require(_migrator != address(0x0), "migrator not set");
+        require(!migrated, "migrated");
+        require(IMigrator(_migrator).want() == address(token), "different token");
         migrator = _migrator;
     }
 
     /**
-     * @dev Updates the end timestamp of the vault.
+     * @dev Updates the timestamp when the vault can be migrated. Cannot be updated after migration.
      */
-    function setVaultEndTimestamp(uint256 _vaultEndTimestamp) public {
-        require(msg.sender == governance, "ACoconutVault: not governance");
-        require(!migrated, "ACoconutVault: migrated");
-        require(_vaultEndTimestamp > block.timestamp, "ACoconutVault: too early");
+    function setMigrationDue(uint256 _migrationDue) public {
+        require(msg.sender == governance, "not governance");
+        require(!migrated, "migrated");
 
-        vaultEndTimestamp = _vaultEndTimestamp;
+        migrationDue = _migrationDue;
     }
 
     /**
      * @dev Performs the migration.
      */
     function migrate() public {
-        require(msg.sender == governance, "ACoconutVault: not governance");
-        require(block.timestamp >= vaultEndTimestamp, "ACoconutVault: too early");
-        require(!migrated, "ACoconutVault: migrated");
-        require(migrator != address(0x0), "ACoconutVault: migrator not set");
+        require(msg.sender == governance, "not governance");
+        require(block.timestamp >= migrationDue, "not due");
+        require(!migrated, "migrated");
+        require(migrator != address(0x0), "migrator not set");
 
         // Final harvest
         IStrategy(strategy).harvest();
@@ -68,48 +69,10 @@ contract ACoconutVault is RewardedVault {
         // Triggers the migration.
         IMigrator(migrator).migrate();
         migrated = true;
-    }
 
-    /**
-     * @dev Deposit some balance into the vault.
-     */
-    function deposit(uint256 amount) public override {
-        require(block.timestamp < vaultEndTimestamp, "ACoconutVault: too late");
-        super.deposit(amount);
-    }
-
-    /**
-     * @dev Deposit all balance into the vault.
-     */
-    function depositAll() public override {
-        require(block.timestamp < vaultEndTimestamp, "ACoconutVault: too late");
-        super.depositAll();
-    }
-
-    /**
-     * @dev Withdraw from the vault. If it's withdrawn before migration, what's withdrawn is the vault token.
-     * If it's withdrawn after migration, what's withdrawn is the migration get token.
-     */
-    function withdraw(uint256 _shares) public override {
-        if (!migrated) {
-            super.withdraw(_shares);
-        } else {
-            // After the migration, the migrator should be always set.
-            IERC20 get = IERC20(IMigrator(migrator).get());
-            uint256 amount = get.balanceOf(address(this)).mul(_shares).div(totalSupply());
-            get.safeTransfer(msg.sender, amount);
-
-            _burn(msg.sender, _shares);
-
-            emit Withdrawn(msg.sender, address(get), amount, _shares);
-        }
-    }
-
-    /**
-     * @dev Withdraw all balance from the vault. If it's withdrawn before migration, what's withdrawn is the vault token.
-     * If it's withdrawn after migration, what's withdrawn is the migration get token.
-     */
-    function withdrawAll() public override {
-        withdraw(balanceOf(msg.sender));
+        // Important: Updates the token and clears the strategy!
+        token = IERC20(IMigrator(migrator).get());
+        // Clears the strategy as token is different.
+        strategy = address(0x0);
     }
 }
