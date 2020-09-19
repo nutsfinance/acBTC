4c15dab4ab660f5e3c50af2e7a912a87b4dabd57
Shengda <dingshengda@gmail.com>
2020-09-19 09:39:46 -0700

    Add test cases for account and account factory

--- test/AccountFactory.test.js
+++ test/AccountFactory.test.js
@@ -5,23 +5,28 @@ const assert = require('assert');
 
 let accountFactory;
 
-contract("AccountFactory", async ([deployer, owner, admin1, admin2]) => {
+contract("AccountFactory", async ([deployer, owner, admin1, admin2, admin3]) => {
     beforeEach(async () => {
-        accountFactory = await AccountFactory.new();
+        const account = await Account.new();
+        accountFactory = await AccountFactory.new(account.address);
     });
     it("should be able to create new account", async () => {
         await accountFactory.createAccount([admin1, admin2], {from: owner});
         const account = await Account.at(await accountFactory.getAccount(owner));
-        assert.equal(await account.hasRole(web3.utils.sha3("OWNER_ROLE"), owner), true);
-        assert.equal(await account.hasRole(web3.utils.sha3("OWNER_ROLE"), deployer), false);
+        assert.equal(await account.owner(), owner);
+        assert.equal(await account.admins(admin1), true);
+        assert.equal(await account.admins(admin2), true);
+        assert.equal(await account.admins(admin3), false);
     });
     it("should be able to create another account", async () => {
         await accountFactory.createAccount([admin1, admin2], {from: owner});
         const account1 = await Account.at(await accountFactory.getAccount(owner));
-        await accountFactory.createAccount([admin1, admin2], {from: owner});
+        await accountFactory.createAccount([admin1, admin3], {from: owner});
         const account2 = await Account.at(await accountFactory.getAccount(owner));
         assert.notEqual(account1, account2);
-        assert.equal(await account2.hasRole(web3.utils.sha3("OWNER_ROLE"), owner), true);
-        assert.equal(await account2.hasRole(web3.utils.sha3("OWNER_ROLE"), deployer), false);
+        assert.equal(await account2.owner(), owner);
+        assert.equal(await account2.admins(admin1), true);
+        assert.equal(await account2.admins(admin2), false);
+        assert.equal(await account2.admins(admin3), true);
     });
 });
