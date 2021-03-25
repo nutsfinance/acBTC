e3b1eb2f842ca97f01c46befd84acf609b5b0431
Shengda <dingshengda@gmail.com>
2021-03-25 13:56:24 -0700

    Update ACoconut Maker

--- contracts/acoconut/ACoconutMaker.sol
+++ contracts/acoconut/ACoconutMaker.sol
@@ -1,107 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.8;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import "./ACoconutSwap.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
- * @notice Contract that collects transaction fees from ACoconutSwap.
+ * @title acBTCx token contract. Collects and distributes acBTC income
+ * to all acBTCx holders.
+ * 
+ * Credit: https://github.com/acBTCswap/acBTCswap/blob/master/contracts/acBTCBar.sol
  */
-contract ACoconutMaker {
-    using SafeERC20 for IERC20;
+contract ACoconutMaker is ERC20("ACoconut Maker", "acBTCx"){
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
-    event FeeCollected(address indexed token, uint256 feeAmount, uint256 feeRewarded, uint256 feeReserved);
+    event Minted(address indexed account, uint256 share, uint256 amount);
+    event Redeemed(address indexed account, uint256 share, uint256 amount);
 
-    address public governance;
+    IERC20 public acBTC;
 
-    address public acBtc;
-    address public acBtcVault;
-    address public acSwap;
-    address public reserve;
-    uint256 public reserveRate = 0;
-    uint256 public constant reserveRateMax = 10000;
-
-    constructor(address _acBtc, address _acBtcVault, address _acSwap) public {
-        require(_acBtc != address(0x0), "acBTC not set");
-        require(_acBtcVault != address(0x0), "acBTC vault not set");
-        require(_acSwap != address(0x0), "acSwap not set");
-
-        acBtc = _acBtc;
-        acBtcVault = _acBtcVault;
-        acSwap = _acSwap;
-        governance = msg.sender;
-        reserve = msg.sender;
+    // Define the acBTC token contract
+    constructor(IERC20 _acBTC) public {
+        acBTC = _acBTC;
     }
 
     /**
-     * @dev Updates the govenance address.
+     * @dev Mints acBTCx with acBTC.
+     * @param _amount Amount of acBTC used to mint acBTCx.
      */
-    function setGovernance(address _governance) external {
-        require(msg.sender == governance, "not governance");
-        governance = _governance;
-    }
+    function mint(uint256 _amount) public {
+        uint256 _totalAmount = acBTC.balanceOf(address(this));
+        uint256 _totalShare = totalSupply();
 
-    /**
-     * @dev Updates the reserve rate.
-     */
-    function setReserveRate(uint256 _reserveRate) external {
-        require(msg.sender == governance, "not governance");
-        require(_reserveRate <= reserveRateMax, "invalid rate");
-
-        reserveRate = _reserveRate;
-    } 
-
-    /**
-     * @dev Updates the reserve address.
-     */
-    function setReserve(address _reserve) external {
-        require(msg.sender == governance, "not governance");
-        require(_reserve != address(0x0), "reserve not set");
-
-        reserve = _reserve;
-    }
-
-    /**
-     * @dev Allocates swap fees accured in the contract.
-     */
-    function allocateFees() public {
-        require(msg.sender == governance, "not governance");
-        uint256 balance = IERC20(acBtc).balanceOf(address(this));
-        uint256 reserveAmount = 0;
-
-        if (balance > 0 && reserveRate > 0 && reserve != address(0x0)) {
-            reserveAmount = balance.mul(reserveRate).div(reserveRateMax);
-            IERC20(acBtc).safeTransfer(reserve, reserveAmount);
-            balance = balance.sub(reserveAmount);
+        uint256 _share = 0;
+        if (_totalAmount == 0 || _totalShare == 0) {
+            _share = _amount;
+        } 
+        else {
+            _share = _amount.mul(_totalShare).div(_totalAmount);
         }
+        _mint(msg.sender, _amount);
+        acBTC.safeTransferFrom(msg.sender, address(this), _amount);
 
-        IERC20(acBtc).safeTransfer(acBtcVault, balance);
-        emit FeeCollected(acBtc, balance.add(reserveAmount), balance, reserveAmount);
-    }
-    
-    /**
-     * @dev Collect fees from the ACoconut Swap.
-     * This contract must be an admin of ACoconut Swap in order to proceed.
-     */
-    function collectFees() public {
-        require(msg.sender == governance, "not governance");
-        ACoconutSwap(acSwap).collectFee();
-        allocateFees();
+        emit Minted(msg.sender, _share, _amount);
     }
 
     /**
-     * @dev Used to salvage any token deposited into the contract by mistake.
-     * @param _tokenAddress Token address to salvage.
-     * @param _amount Amount of token to salvage.
+     * @dev Redeems acBTCx to acBTC.
+     * @param _share Amount of acBTCx to redeem.
      */
-    function salvage(address _tokenAddress, uint256 _amount) public {
-        require(msg.sender == governance, "not governance");
-        require(_tokenAddress != acBtc, "cannot salvage");
-        require(_amount > 0, "zero amount");
-        IERC20(_tokenAddress).safeTransfer(governance, _amount);
+    function redeem(uint256 _share) public {
+        uint256 _totalAmount = acBTC.balanceOf(address(this));
+        uint256 _totalShare = totalSupply();
+        uint256 _amount = _share.mul(_totalAmount).div(_totalShare);
+        _burn(msg.sender, _share);
+        acBTC.safeTransfer(msg.sender, _amount);
+
+        emit Redeemed(msg.sender, _share, _amount);
     }
 }
